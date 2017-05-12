e83814fb4455f1953251755bdfdae45431db8cab
minomonter <KojiMinoda@KojiMinoda-no-MacBook-Pro.local>
2017-05-12 09:42:16 +0900

    added a new file

---
+++ swing-by.py
@@ -0,0 +1,22 @@
+ # -*- coding: utf-8 -*-
+
+
+class Planet():
+    def __init__(self,radius,mass):
+        self.radius = radius
+        self.mass = mass
+        
+
+mercury = Planet()
+venus = Planet()
+earth = Planet()
+mars = Planet()
+jupiter = Planet()
+saturn = Planet()
+uranus = Planet()
+neptune = Planet()
+pluto = Planet()
+
+if __name__ == "__main__":
+
+

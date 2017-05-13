928a51452fe7bbeacee58ac2d5e17185d8006829
1510099204 <1510099204@pc111090m.ecc.u-tokyo.ac.jp>
2017-05-13 14:39:03 +0900

    added a class

--- swing-by_planet.py
+++ swing-by_planet.py
@@ -5,7 +5,13 @@ class Planet():
     def __init__(self,radius,mass):
         self.radius = radius
         self.mass = mass
-        
+
+
+class Spacecraft():
+    def __init__(self,mass):
+        self.mass = mass
+
+    def 
 
 if __name__ == "__main__":
 

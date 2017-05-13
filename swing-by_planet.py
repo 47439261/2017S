fbece65923c5a08f58c5d3f1760e16e7d9d98276
1510099204 <1510099204@pc111090m.ecc.u-tokyo.ac.jp>
2017-05-13 14:42:50 +0900

    added two functions

--- swing-by_planet.py
+++ swing-by_planet.py
@@ -6,12 +6,18 @@ class Planet():
         self.radius = radius
         self.mass = mass
 
+    def location():
+    """惑星の座標を返す"""
+
+    def distance(self,x,y,z,t):
+    """時刻と宇宙機の座標から、宇宙機とその惑星間の距離を返す"""
+
 
 class Spacecraft():
     def __init__(self,mass):
         self.mass = mass
 
-    def 
+    def
 
 if __name__ == "__main__":
 

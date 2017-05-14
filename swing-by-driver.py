0a45d4306cc8a07bbcde9d230b68c725d7d41d8b
minomonter <KojiMinoda@KojiMinoda-no-MacBook-Pro.local>
2017-05-14 17:20:43 +0900

    added a file

---
+++ swing-by-driver.py
@@ -0,0 +1,17 @@
+ # -*- coding: utf-8 -*-
+import swing-by_planet
+
+
+mercury = Planet()
+venus = Planet()
+earth = Planet()
+mars = Planet()
+jupiter = Planet()
+saturn = Planet()
+uranus = Planet()
+neptune = Planet()
+pluto = Planet()
+
+if __name__ == "__main__":
+
+
